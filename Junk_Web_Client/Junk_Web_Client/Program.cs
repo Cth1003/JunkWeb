35c85e1d2c1b3eb06fc4a3a6758b2325096287fa
Cth1003 <ItsAMeCth@gmail.com>
2016-06-27 11:13:45 +0000

    Added More IP's

--- Junk_Web_Client/Junk_Web_Client/Program.cs
+++ Junk_Web_Client/Junk_Web_Client/Program.cs
@@ -25,7 +25,7 @@ namespace Junk_Web_Client
             {
                 //jump to dead ip address so much junk
                 Random rnd = new Random();
-                string[] ip = new string[] { "https://81.196.2.213:80/", "https://203.66.159.44:80/", "https://123.57.190.51:80/", "https://123.57.190.51:80/", "https://199.16.220.249:80/", "https://202.47.236.251:80/", "https://202.47.236.252/", "https://203.66.159.46:80/" };
+                string[] ip = new string[] { "https://81.196.2.213:80/", "https://203.66.159.44:80/", "https://123.57.190.51:80/", "https://123.57.190.51:80/", "https://199.16.220.249:80/", "https://202.47.236.251:80/", "https://202.47.236.252/", "https://203.66.159.46:80/","https://82.166.160.30:80/","https://217.175.34.170:80","https://123.57.190.51:80/" };
                 string res = ip[rnd.Next(ip.Length)];
                 WebProxy proxyObject = new WebProxy(res);
                 GlobalProxySelection.Select = proxyObject;
