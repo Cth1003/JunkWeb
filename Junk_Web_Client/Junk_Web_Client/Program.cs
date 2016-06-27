dc95310bafbc1b3c8f690482936ab3c5aa411b5d
Cth1003 <ItsAMeCth@gmail.com>
2016-06-27 11:43:20 +0000

    Update Program.cs

--- Junk_Web_Client/Junk_Web_Client/Program.cs
+++ Junk_Web_Client/Junk_Web_Client/Program.cs
@@ -19,19 +19,28 @@ namespace Junk_Web_Client
         //Main
         static void Main(string[] args)
         {
+           //write to console "IP SWITCHER ... START"
             Console.WriteLine("IP switcher... start");
+            //creates the i variables
             int i = 0;
             while (i < 10)
             {
                 //jump to dead ip address so much junk
+                //random
                 Random rnd = new Random();
+                //all the deads ips on array
                 string[] ip = new string[] { "https://81.196.2.213:80/", "https://203.66.159.44:80/", "https://123.57.190.51:80/", "https://123.57.190.51:80/", "https://199.16.220.249:80/", "https://202.47.236.251:80/", "https://202.47.236.252/", "https://203.66.159.46:80/","https://82.166.160.30:80/","https://217.175.34.170:80","https://123.57.190.51:80/" };
+                //select random string from array
                 string res = ip[rnd.Next(ip.Length)];
                 res = ip[rnd.Next(ip.Length)];
+                //just set current proxy to ip
                 WebProxy proxyObject = new WebProxy(res);
                 GlobalProxySelection.Select = proxyObject;
+                //well. displays "switching to ip : ip"
                 Console.WriteLine("Switching to IP : "+res);
+                //add 1 to i
                 i++;
+                //if i is 10. take a break
                 if (i == 10)
                 {
                     Web();
@@ -42,20 +51,20 @@ namespace Junk_Web_Client
 
         private static void Web()
         {
-            //The Web
+            //proxy pause
             Console.WriteLine("Proxy pause...");
+            //wait for 10000 milliseconds
             System.Threading.Thread.Sleep(10000);
+            //reset the variable
+            i = 0;
+            System.Threading.Thread.Sleep(1000);
+            proxyObject = null;
+            Console.WriteLine("disable proxy");
+            System.Threading.Thread.Sleep(1000);
             Main("-restarted")
+            
         }
-        private void Form1_FormClosing( object sender, FormClosingEventArgs e ) {
-             //DONT FORGET TO KILL THE PROXY
-            var window = MessageBox.Show( "This will end the proxy ! End anyways ?", buttons: MessageBoxButtons.YesNo );
-            if (window == DialogResult.No) e.Cancel = true;
-            else 
-            {
-	         proxyObject = null;
-	         e.Cancel = false;
-            }
+	        
         }
     }
 
